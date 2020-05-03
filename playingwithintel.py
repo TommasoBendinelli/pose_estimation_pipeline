68cd198388e2645d095e699fae5e68b62c34f3f2
TommasoBendinelli <tommaso.bendinelli@gmail.com>
2020-05-03 19:50:30 +0200

    yolact working

---
+++ playingwithintel.py
@@ -0,0 +1,5 @@
+import pyrealsense2 as rs
+pipeline = rs.pipeline()
+config = rs.config()
+config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
+config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
